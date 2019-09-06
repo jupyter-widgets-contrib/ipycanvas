bd2985c2ddc501ec7db5e4be122b1484e4709a3b
martinRenou <martin.renou@gmail.com>
2019-09-06 17:11:12 +0200

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ setup_args = dict(
     ],
     include_package_data = True,
     install_requires = [
-        'ipywidgets>=7.0.0',
+        'ipywidgets>=7.5.0',
     ],
     extras_require = {
         'examples': [
