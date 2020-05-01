4d1fdea1b42bd963115cb881ee9d0810ef2a9913
martinRenou <martin.renou@gmail.com>
2020-05-01 09:05:31 +0200

    Add support for int8 int16, float16 float32 float64 buffer arrays
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,19 @@
 function getTypedArray(dataview: any, metadata: any) {
   switch (metadata.dtype) {
+    case 'int8':
+      return new Int8Array(dataview.buffer);
+      break;
+    case 'int16':
+      return new Int16Array(dataview.buffer);
+      break;
     case 'int32':
       return new Int32Array(dataview.buffer);
+      break;
+    case 'float32':
+      return new Float32Array(dataview.buffer);
+      break;
+    case 'float64':
+      return new Float64Array(dataview.buffer);
       break;
     default:
       throw 'Unknown dtype ' + metadata.dtype;
