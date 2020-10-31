d6067ca15b1c25b03b1f2307b302b74b1f77dda3
martinRenou <martin.renou@gmail.com>
2020-10-31 12:34:41 +0100

    Release 0.6.0
    Signed-off-by: martinRenou <martin.renou@gmail.com>

--- ipycanvas/_version.py
+++ ipycanvas/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Martin Renou.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 5, 1)
+version_info = (0, 6, 0)
 __version__ = ".".join(map(str, version_info))
