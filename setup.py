e28157cb0e2d03c898827154b35e2a8df4652236
martinRenou <martin.renou@gmail.com>
2021-05-08 10:57:14 +0200

    Modern packaging

--- setup.py
+++ setup.py
@@ -5,12 +5,10 @@
 # Distributed under the terms of the Modified BSD License.
 
 from __future__ import print_function
-from glob import glob
 from os import path
 
 
 from jupyter_packaging import (
-    ensure_python,
     get_version,
     wrap_installers,
     npm_builder,
@@ -24,9 +22,6 @@ HERE = path.dirname(path.abspath(__file__))
 
 # The name of the project
 name = 'ipycanvas'
-
-# Ensure a valid python version
-ensure_python('>=3.5')
 
 # Get our version
 version = get_version(path.join(name, '_version.py'))
@@ -58,39 +53,10 @@ cmdclass = wrap_installers(
 )
 
 setup_args = dict(
-    name=name,
-    description='Interactive widgets library exposing the browser\'s Canvas API',
     version=version,
-    scripts=glob(path.join('scripts', '*')),
     cmdclass=cmdclass,
     data_files=get_data_files(data_files_spec),
-    packages=find_packages(),
-    author='Martin Renou',
-    author_email='martin.renou@gmail.com',
-    url='https://github.com/martinRenou/ipycanvas',
-    license='BSD',
-    platforms="Linux, Mac OS X, Windows",
-    keywords=['Jupyter', 'Widgets', 'IPython'],
-    classifiers=[
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Framework :: Jupyter',
-    ],
-    include_package_data=True,
-    install_requires=[
-        'ipywidgets>=7.6.0',
-        'pillow>=6.0',
-        'numpy',
-        'orjson'
-    ],
+    packages=find_packages()
 )
 
 if __name__ == '__main__':
