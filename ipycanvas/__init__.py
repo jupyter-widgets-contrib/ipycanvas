2c0a80ec09c0427df45ccff46bb36238c77fb3e3
martinRenou <martin.renou@gmail.com>
2021-05-08 10:17:21 +0200

    Allow for labextension develop

--- ipycanvas/__init__.py
+++ ipycanvas/__init__.py
@@ -7,4 +7,18 @@
 from .canvas import Path2D, Canvas, RoughCanvas, MultiCanvas, MultiRoughCanvas, hold_canvas  # noqa
 from ._version import __version__, version_info  # noqa
 
-from .nbextension import _jupyter_nbextension_paths  # noqa
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'nbextension/static',
+        'dest': 'ipycanvas',
+        'require': 'ipycanvas/extension'
+    }]
+
+
+def _jupyter_labextension_paths():
+    return [{
+        'src': 'labextension',
+        'dest': 'ipycanvas',
+    }]
