3ec976aab3f9310eaec28ce1f8dff37436566570
martinRenou <martin.renou@gmail.com>
2021-12-01 09:29:48 +0100

    Release 0.10.2

--- ipycanvas/_version.py
+++ ipycanvas/_version.py
@@ -4,5 +4,5 @@
 # Copyright (c) Martin Renou.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 10, 1)
+version_info = (0, 10, 2)
 __version__ = ".".join(map(str, version_info))
