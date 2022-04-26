b72433470b830783a9006ba3d799b19e02c94179
martinRenou <martin.renou@gmail.com>
2022-04-26 10:35:09 +0200

    Add linters

--- src/extension.ts
+++ src/extension.ts
@@ -8,6 +8,8 @@
 // Some static assets may be required by the custom widget javascript. The base
 // url for the notebook is not known at build time and is therefore computed
 // dynamically.
-(window as any).__webpack_public_path__ = document.querySelector('body')!.getAttribute('data-base-url') + 'nbextensions/ipycanvas';
+(window as any).__webpack_public_path__ =
+  document.querySelector('body')!.getAttribute('data-base-url') +
+  'nbextensions/ipycanvas';
 
 export * from './index';
