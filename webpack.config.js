8276256e5867990a1f8d1d8a6652015585a08a6f
martinRenou <martin.renou@gmail.com>
2022-08-19 10:27:07 +0200

    Remove doc bundle
    We never use this

--- webpack.config.js
+++ webpack.config.js
@@ -68,26 +68,5 @@ module.exports = [
     },
     externals,
     resolve
-  },
-
-  /**
-   * Documentation widget bundle
-   *
-   * This bundle is used to embed widgets in the package documentation.
-   */
-  {
-    entry: './src/index.ts',
-    output: {
-      filename: 'embed-bundle.js',
-      path: path.resolve(__dirname, 'docs', 'source', '_static'),
-      library: 'ipycanvas',
-      libraryTarget: 'amd'
-    },
-    module: {
-      rules: rules
-    },
-    devtool: 'source-map',
-    externals,
-    resolve
   }
 ];
