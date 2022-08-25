4f1fe1356527446797bc56bc7b6ac51524e9e59b
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-08-25 12:24:58 +0200

    Make touch example work (#281)
    * make touch example work
    
    * Linters
    
    Co-authored-by: martinRenou <martin.renou@gmail.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,38 +1,30 @@
 # -*- coding: utf-8 -*-
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'jupyterlite_sphinx'
-]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon", "jupyterlite_sphinx"]
 
 jupyterlite_config = "jupyterlite_config.json"
 jupyterlite_dir = "."
 
-master_doc = 'index'
-source_suffix = '.rst'
+master_doc = "index"
+source_suffix = ".rst"
 
 # General information about the project.
-project = 'ipycanvas'
-author = 'Martin Renou'
+project = "ipycanvas"
+author = "Martin Renou"
 
 exclude_patterns = []
-highlight_language = 'python'
-pygments_style = 'sphinx'
+highlight_language = "python"
+pygments_style = "sphinx"
 
 # Output file base name for HTML help builder.
 html_logo = "./images/ipycanvas_logo.svg"
 html_favicon = "./images/ipycanvas_logo.ico"
 html_theme = "pydata_sphinx_theme"
-htmlhelp_basename = 'ipycanvasdoc'
+htmlhelp_basename = "ipycanvasdoc"
 
-html_theme_options = dict(
-    github_url='https://github.com/martinRenou/ipycanvas'
-)
+html_theme_options = dict(github_url="https://github.com/martinRenou/ipycanvas")
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
-html_css_files = [
-    'custom.css'
-]
+html_css_files = ["custom.css"]
 
-autodoc_member_order = 'bysource'
+autodoc_member_order = "bysource"
