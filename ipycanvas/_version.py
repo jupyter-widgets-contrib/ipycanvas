3be69ca5c7a894ed1222e24df164b5b597423c95
martinRenou <martin.renou@gmail.com>
2022-10-20 13:30:07 +0200

    Hatch migration

--- ipycanvas/_version.py
+++ ipycanvas/_version.py
@@ -4,5 +4,4 @@
 # Copyright (c) Martin Renou.
 # Distributed under the terms of the Modified BSD License.
 
-version_info = (0, 13, 1)
-__version__ = ".".join(map(str, version_info))
+__version__ = "0.13.1"
