eae569f43ecb7007c4a15e23cffde2b441df9d20
DerThorsten <derthorstenbeier@gmail.com>
2025-07-07 14:27:50 +0200

    mouse events work

--- src/offscreen.ts
+++ src/offscreen.ts
@@ -7,18 +7,19 @@ import {
 import { MODULE_NAME, MODULE_VERSION } from './version';
 
 // MODEL
- class MyCanvasModel extends DOMWidgetModel {
+ class OffscreenCanvasModel extends DOMWidgetModel {
   defaults() {
     return {
       ...super.defaults(),
-      _model_name: MyCanvasModel.model_name,
-      _model_module: MyCanvasModel.model_module,
-      _model_module_version: MyCanvasModel.model_module_version,
-      _view_name: MyCanvasModel.view_name,
-      _view_module: MyCanvasModel.view_module,
-      _view_module_version: MyCanvasModel.view_module_version,
+      _model_name: OffscreenCanvasModel.model_name,
+      _model_module: OffscreenCanvasModel.model_module,
+      _model_module_version: OffscreenCanvasModel.model_module_version,
+      _view_name: OffscreenCanvasModel.view_name,
+      _view_module: OffscreenCanvasModel.view_module,
+      _view_module_version: OffscreenCanvasModel.view_module_version,
       width: 300,
       height: 150,
+      _name: OffscreenCanvasModel._name
     };
   }
 
@@ -26,36 +27,80 @@ import { MODULE_NAME, MODULE_VERSION } from './version';
     ...DOMWidgetModel.serializers,
   };
 
-  static model_name = 'MyCanvasModel';
+  static model_name = 'OffscreenCanvasModel';
   static model_module = MODULE_NAME;
   static model_module_version = MODULE_VERSION;
-  static view_name = 'MyCanvasView';
+  static view_name = 'OffscreenCanvasView';
   static view_module = MODULE_NAME;
   static view_module_version = MODULE_VERSION;
+  static _name = "_canvas_0";
 }
 
 // VIEW
-class MyCanvasView extends DOMWidgetView {
-  canvasEl: HTMLCanvasElement;
+class OffscreenCanvasView extends DOMWidgetView {
+  canvas: HTMLCanvasElement;
 
   render(): void {
-    this.canvasEl = document.createElement('canvas');
-    this.el.appendChild(this.canvasEl);
+    this.canvas = document.createElement('canvas');
+    this.el.appendChild(this.canvas);
 
-    this.updateCanvasSize();
+
+    const _canvas_name = () => `_canvas_${this.model.get('_name')}`;
+    const _reciver_name = () => `_canvas_reciver_${this.model.get('_name')}`;
+
+    // magic here!
+    const offscreen: OffscreenCanvas = this.canvas.transferControlToOffscreen();
+    (globalThis as any).storeAsGlobal(offscreen, _canvas_name());
+    
+
+    const that = this;
+
+    async function sendMouseEvent(event : MouseEvent): Promise<void> {
+
+        const rect = that.canvas.getBoundingClientRect();
+        try{
+            await (globalThis as any).callGlobalReciver(_reciver_name(), "on_mouse_events",
+                event.type,
+                event.clientX - rect.left,
+                event.clientY - rect.top,
+            );
+        } 
+        // we want to remove all event listeners if the reciver is not defined
+        catch (e) {
+            console.error("Error while sending mouse event, removing listeners:", e);
+            (that as any).canvas.removeEventListener("mousedown", sendMouseEvent);
+            (that as any).canvas.removeEventListener("mouseup",   sendMouseEvent);
+            (that as any).canvas.removeEventListener("mousemove", sendMouseEvent);        
+            (that as any).canvas.removeEventListener("mouseenter", sendMouseEvent);
+            (that as any).canvas.removeEventListener("mouseleave", sendMouseEvent);
+        }
+
+    };
+
+    
+    this.canvas.addEventListener("mousedown", sendMouseEvent);
+    this.canvas.addEventListener("mouseup",   sendMouseEvent);
+    this.canvas.addEventListener("mousemove", sendMouseEvent);
+    this.canvas.addEventListener("mouseenter", sendMouseEvent);
+    this.canvas.addEventListener("mouseleave", sendMouseEvent);
+
+
+
+
 
     this.model.on('change:width', this.updateCanvasSize, this);
     this.model.on('change:height', this.updateCanvasSize, this);
   }
 
+
   updateCanvasSize(): void {
     const width = this.model.get('width');
     const height = this.model.get('height');
-    this.canvasEl.width = width;
-    this.canvasEl.height = height;
-    this.canvasEl.style.width = `${width}px`;
-    this.canvasEl.style.height = `${height}px`;
+    this.canvas.width = width;
+    this.canvas.height = height;
+    this.canvas.style.width = `${width}px`;
+    this.canvas.style.height = `${height}px`;
   }
 }
 
-export { MyCanvasModel, MyCanvasView };
+export { OffscreenCanvasModel, OffscreenCanvasView };
