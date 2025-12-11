482f6e8bae2aeb833fdfebc672be41d141b8f9c6
martinRenou <martin.renou@gmail.com>
2025-12-11 10:43:01 +0100

    Fix unpacking for when there will be alpha releases

--- ipycanvas/_frontend.py
+++ ipycanvas/_frontend.py
@@ -10,7 +10,7 @@ Information about the frontend package of the widgets.
 
 from ._version import __version__
 
-major, minor, patch = __version__.split('.')
+major, minor, *_ = __version__.split('.')
 
 module_name = "ipycanvas"
 module_version = f"^{major}.{minor}"
